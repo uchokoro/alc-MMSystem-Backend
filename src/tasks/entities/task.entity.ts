31391efdac315f824688cf7f6b83618e5823fc76
uchokoro <96576666+uchokoro@users.noreply.github.com>
2022-11-22 10:17:29 +0100

    Completed the task entity

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,7 +1,74 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+import { Programme } from '../../programmes/entities/programme.entity';
+
+export enum TaskStatus {
+  COMPLETED = 'completed',
+  IN_PROGRESS = 'in_progress',
+  PENDING = 'pending',
+  UNASSIGNED = 'unassigned',
+}
 
 @Entity('tasks')
 export class Task {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar' })
+  title: string;
+
+  @Column({ type: 'varchar' })
+  description: string;
+
+  @Column({ type: 'enum', enum: TaskStatus, default: TaskStatus.UNASSIGNED, })
+  status: TaskStatus;
+
+  @ManyToOne(() => Programme, (programme) => programme.tasks, { cascade: true, })
+  programme: Programme;
+
+  @OneToOne(() => User)
+  @JoinColumn()
+  created_by: User;
+
+  @Column()
+  @CreateDateColumn()
+  created_at: Date;
+
+  @OneToOne(() => User, { nullable: true, })
+  @JoinColumn()
+  assigned_to: User;
+
+  @OneToOne(() => User, { nullable: true, })
+  @JoinColumn()
+  assigned_by: User;
+
+  @Column({ type: 'datetime', nullable: true})
+  assigned_at: Date;
+
+  @OneToOne(() => User)
+  @JoinColumn()
+  last_updated_by: string;
+
+  @Column()
+  @UpdateDateColumn()
+  last_updated_at: Date;
+
+  @OneToOne(() => User, { nullable: true, })
+  @JoinColumn()
+  deleted_by: string;
+
+  @Column()
+  @DeleteDateColumn()
+  deleted_at: Date;
+
 }
