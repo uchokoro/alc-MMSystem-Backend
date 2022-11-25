344f4b7226e67b49e01ee1ce02a23a514e4ebede
timosamedet <timosamedet@yahoo.com>
2022-11-25 06:07:05 +0100

    imported TypeOrmModule in the Report Module file.

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateReportDto } from './dto/create-report.dto';
 import { UpdateReportDto } from './dto/update-report.dto';
+import { Report } from './entities/report.entity';
 
 @Injectable()
 export class ReportsService {
+  constructor(
+    @InjectRepository(Report) private reportRepository: Repository<Report>,
+  ) {}
   create(createReportDto: CreateReportDto) {
     return createReportDto;
   }
