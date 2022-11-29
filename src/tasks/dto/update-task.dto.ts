1912b5915bce2dbcb0b06f5f7b7621e99f1688de
eadortsu <eugeneadortsu@gmail.com>
2022-11-29 09:35:32 +0000

    fix codacy errors

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,8 +1,6 @@
 import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { BaseTaskDto } from './base-task.dto';
 import { TaskStatus } from '../entities/task.entity';
-import { Programme } from '../../programmes/entities/programme.entity';
-import { User } from '../../users/entities/user.entity';
 import { IsNotEmpty } from 'class-validator';
 
 export class UpdateTaskDto extends PartialType(BaseTaskDto) {
